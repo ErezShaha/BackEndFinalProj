3ffdb33ac31d6226e17288422090e116532475ca
TheAssaf <assaf.hadad@gmail.com>
2025-01-01 16:19:24 +0200

    dms added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import { config } from 'dotenv';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import userRouter from './routers/userRouter.js';
-import http from 'http';
-import { Server } from'socket.io';
+import express from "express";
+import { config } from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter.js";
+import http from "http";
+import { Server } from "socket.io";
 
 config();
 
@@ -13,379 +13,179 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: "*",
+  cors: "*",
 });
 
 export const onlineUsers = {};
+const onlineUsersByUsername = {};
 const openRooms = {};
 const roomChats = {};
 const chatRoomNumber = 0;
 
 io.on("connection", (socket) => {
-    console.log(`Socket ${socket.id} Connected`); 
+  console.log(`Socket ${socket.id} Connected`);
 
+  // LogIn
+  socket.on("UserLogin", (username) => {
+    onlineUsers[socket.id] = { username: username };
+    onlineUsersByUsername[username] = { socketID: socket.id };
+    console.log(`Socket ${socket.id} Loggedin To User ${username}`);
 
-    // LogIn
-    socket.on("UserLogin", (username) => {
-        onlineUsers[socket.id] = {username: username};
-        onlineUsers[username] = {socketID: socket.id};
-        console.log(`Socket ${socket.id} Loggedin To User ${username}`);
-    
-        // onlineChange
-        console.log(onlineUsers);
-        io.emit("hereTakeYourUser", Object.values(onlineUsers));
-    });
+    // onlineChange
+    console.log(onlineUsers);
+    io.emit("hereTakeYourUser", Object.values(onlineUsers));
+  });
 
+  // LogOut
+  socket.on("UserLogout", () => {
+    console.log(
+      `Socket ${socket.id} Loggedout from User ${onlineUsers[socket.id]}`
+    );
 
-    // LogOut
-    socket.on("UserLogout", () => {
-        console.log(`Socket ${socket.id} Loggedout from User ${onlineUsers[socket.id]}`);
+    delete onlineUsers[socket.id];
+    console.log(`Jobs Done. ${onlineUsers}`);
 
-        delete onlineUsers[socket.id];
-        console.log(`Jobs Done. ${onlineUsers}`);
-        
-        // onlineChange
-        console.log(onlineUsers);
-        io.emit("hereTakeYourUser", Object.values(onlineUsers));
-    });
-    
+    // onlineChange
+    console.log(onlineUsers);
+    io.emit("hereTakeYourUser", Object.values(onlineUsers));
+  });
 
-    // Disconnect
-    socket.on("disconnect", () => {
-        console.log("======================Disconnect Start==========================");
-        console.log(`Socket ${socket.id} disconnected. Logging out from user ${onlineUsers[socket.id]}`);
+  // Disconnect
+  socket.on("disconnect", () => {
+    console.log(
+      "======================Disconnect Start=========================="
+    );
+    console.log(
+      `Socket ${socket.id} disconnected. Logging out from user ${
+        onlineUsers[socket.id]
+      }`
+    );
+    const disconnectingUser = onlineUsers[socket.id];
 
-        for (const [roomNumber, usersInRoom] of Object.entries(openRooms)) {
-            if (usersInRoom.includes(onlineUsers[socket.id].username)) {
-                
-            }
+    for (const [roomNumber, usersInRoom] of Object.entries(openRooms)) {
+      if (usersInRoom.includes(disconnectingUser.username)) {
+        const otherUser = usersInRoom.find(
+          (u) => u !== disconnectingUser.username
+        );
+
+        if (!onlineUsersByUsername[otherUser]) {
+          delete openRooms[roomNumber];
+          delete roomChats[roomNumber];
         }
+      }
+    }
 
+    delete onlineUsersByUsername[onlineUsers[socket.id].username];
+    delete onlineUsers[socket.id];
 
-        //check if the users taht have shared rooms are still online if not delete them from the room list
-        // if(openRooms[onlineUsers[socket.id]]){
-            
-        //     // console.log(onlineRooms);
-        //     // onlineRooms[onlineUsers[socket.id]].forEach(userConnection => {
-        //     //     const indexToRemove = onlineRooms[userConnection.username].findIndex(roomConnection => roomConnection.username === userConnection.username);
-        //     //     onlineRooms[userConnection.username].splice(indexToRemove, 1);
-        //     // });
-        //     // console.log(onlineRooms);
-        //     // delete onlineRooms[onlineUsers[socket.id]];
-        //     console.log(openRooms);
-        // }
-        delete onlineUsers[onlineUsers[socket.id].username];
-        delete onlineUsers[socket.id];
-        
-        // onlineChange
-        console.log(onlineUsers);
-        io.emit("hereTakeYourUser", Object.values(onlineUsers));
-        console.log("======================Disconnect End==========================");
-    });
-    
+    // onlineChange
+    console.log(onlineUsers);
+    io.emit("hereTakeYourUser", Object.values(onlineUsers));
+    console.log(
+      "======================Disconnect End=========================="
+    );
+  });
 
-    
-    socket.on("giveMeMyUser", () => {
-        console.log(onlineUsers);
-        io.emit("hereTakeYourUser", Object.values(onlineUsers));          
-    });
+  socket.on("giveMeMyUser", () => {
+    console.log(onlineUsers);
+    io.emit("hereTakeYourUser", Object.values(onlineUsers));
+  });
 
-    
-    socket.on("lookAtOnlineUsers", () => {
-        console.log(onlineUsers);
-    });
-    
-    socket.on("CheckLoggedin", (username) => {
-        if(onlineUsers[socket.id] === undefined) {
-            onlineUsers[socket.id] = {username: username};
-            console.log("good job saved dab");
-            console.log(onlineUsers);
-        };
-        io.emit("hereTakeYourUser", Object.values(onlineUsers));  
-    });
+  socket.on("lookAtOnlineUsers", () => {
+    console.log(onlineUsers);
+  });
 
+  socket.on("CheckLoggedin", (username) => {
+    if (!onlineUsers[socket.id]) {
+      onlineUsers[socket.id] = { username: username };
+      onlineUsersByUsername[username] = { socketID: socket.id };
+      console.log("good job saved dab");
+      console.log(onlineUsers);
+    }
+    io.emit("hereTakeYourUser", Object.values(onlineUsers));
+  });
 
+  socket.on("SendMessageToEveryone", (message) => {
+    const now = new Date();
+    const messageObject = {
+      id: Date.now(),
+      content: message,
+      user: onlineUsers[socket.id],
+      msgTime: `${now.getHours().toString().padStart(2, "0")}:${now
+        .getMinutes()
+        .toString()
+        .padStart(2, "0")}`,
+    };
 
+    io.emit("RecieveMessage", messageObject);
+  });
 
+  socket.on("SendMessageToRoom", (room, message) => {
+    const now = new Date();
+    const messageObject = {
+      id: Date.now(),
+      content: message,
+      user: onlineUsers[socket.id],
+      msgTime: `${now.getHours().toString().padStart(2, "0")}:${now
+        .getMinutes()
+        .toString()
+        .padStart(2, "0")}`,
+    };
 
+    roomChats[room] = [...(roomChats[room] || []), messageObject];
+    io.to(room).emit("RecieveDmMessage", roomChats[room]);
+  });
 
-    socket.on("SendMessageToEveryone", (message) => {
-        const now = new Date();
-        const messageObject = {id: Date.now(), content: message , user: onlineUsers[socket.id], msgTime: `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`};
-        io.emit("RecieveMessage", messageObject);
-    })
+  socket.on("JoinRoom", (room) => {
+    socket.join(room);
+  });
 
-    socket.on("SendMessageToRoom", (room, message) => {
-        const now = new Date();
-        const messageObject = {id: Date.now(), content: message , user: onlineUsers[socket.id], msgTime: `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`};
-        roomChats[room] = [...roomChats[room] || [], messageObject];
-        io.to(room).emit("RecieveDmMessage", roomChats[room]);
+  socket.on("ChatRoom", (secondUser) => {
+    const secondUserSocketID = onlineUsersByUsername[secondUser].socketID;
+    console.log("secondUserSocketID", secondUserSocketID);
 
-        //on the other side
-        // socket.on("SendMessageToRoom", (message) => {
-            // setMsgDM([...msgDM, message]);
-        // })
-    });
-
-    socket.on("JoinRoom", (room) => {
-        socket.join(room);
-    });
-
-    socket.on("ChatRoom", (secondUser) => {
-        const secondUserSocketID = Object.values(onlineUsers).findIndex(user => user.username === secondUser);
-        console.log("secondUserSocketID", secondUserSocketID);
-
-        for (const [roomNumber, usersInRoom] of Object.entries(openRooms)) {
-            if (usersInRoom.includes(secondUser) && usersInRoom.includes(onlineUsers[socket.id].username)) {
-                io.to(socket.id).emit("RoomNumberForUser", secondUser, roomNumber);
-                io.to(onlineUsers[secondUserSocketID]).emit("RoomNumberForUser", onlineUsers[socket.id].username, roomNumber);
-                return;
-            }
-        }
-        console.log(chatRoomNumber);
-        openRooms[++chatRoomNumber] = [onlineUsers[socket.id].username, secondUser];
-        console.log(chatRoomNumber);
-
+    for (const [roomNumber, usersInRoom] of Object.entries(openRooms)) {
+      if (
+        usersInRoom.includes(secondUser) &&
+        usersInRoom.includes(onlineUsers[socket.id].username)
+      ) {
         io.to(socket.id).emit("RoomNumberForUser", secondUser, roomNumber);
-        io.to(onlineUsers[secondUserSocketID]).emit("RoomNumberForUser", onlineUsers[socket.id].username, roomNumber);
-    });
-    
-    // socket.on("InviteUserToChat", (inviteUsername) => {
+        io.to(onlineUsers[secondUserSocketID]).emit(
+          "RoomNumberForUser",
+          onlineUsers[socket.id].username,
+          roomNumber
+        );
+        return;
+      }
+    }
+    console.log(chatRoomNumber);
+    openRooms[++chatRoomNumber] = [onlineUsers[socket.id].username, secondUser];
+    console.log(chatRoomNumber);
 
-    //     )
-
-    // socket.on("JoinRoomForTwo", (invitedUsername) => {
-    //     console.log("======================Join Room For Two Start==========================");
-    //     const sharedRoom = Object.values(onlineRooms[onlineUsers[socket.id]]).find(roomConnections => roomConnections.username === invitedUsername).room;
-    //     if(!sharedRoom) {
-    //         sharedRoom = onlineUsers[socket.id].username + invitedUsername;
-    //         onlineRooms[onlineUsers[socket.id]].push({username: invitedUsername, room: roomName});
-    //         onlineRooms[invitedUsername].push({username: onlineUsers[socket.id].username, room: roomName});
-    //         socket.join(roomName);
-    //     } else {
-    //         socket.join(sharedRoom);
-    //     }
-    //     io.to(inviteUserSocketID).emit("BoBoLahederHabibi", sharedRoom);
-    //     console.log("======================Join Room For Two End==========================");
-
-    //     //on the other side
-    //     // socket.on("BoBoLahederHabibi", (room) => {
-    //         // socket.join(room);
-    //         // setCorrentRoom(room);
-    //         // console.log("joined room");
-    //     // })
-    // });
-
-    // const [currentRoom, setCorrentRoom] = useState();
-    // const [msgDM, setMsgDM] = useState([]);
-
-
+    io.to(socket.id).emit("RoomNumberForUser", secondUser, roomNumber);
+    io.to(onlineUsers[secondUserSocketID]).emit(
+      "RoomNumberForUser",
+      onlineUsers[socket.id].username,
+      roomNumber
+    );
+  });
 });
 
-
-
-
-
-
-
-
-
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/users", userRouter);
 //app.use('/api/v1/lobby', lobbyRouter);
 
-
-mongoose.connect(process.env.MONGO_CONNECTION)
-    .then(() => {
-        server.listen(PORT, () => {
-            console.log(`Listening on PORT ${PORT}`);
-        })
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//pre soketim
-
-//     config();
-
-// const app = express();
-// const PORT = process.env.PORT || 8080;
-
-// app.use(cors({origin: "http://localhost:5173", credentials: true}));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-
-// app.use('/api/v1/users', userRouter);
-// app.use('/api/v1/users', userRouter);
-
-
-// mongoose.connect(process.env.MONGO_CONNECTION)
-//     .then(() => {
-//         app.listen(PORT, () => {
-//             console.log(`Listening on PORT ${PORT}`);
-//         })
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     })
-
-
-// <Form onSubmit={login}>
-//           <Form.Group>
-//           <InputGroup className="mb-3">
-//         <Form.Control
-//         value={user.username}
-//         onChange={(e) => setuser({...user, username: e.target.value})}
-//           placeholder="Username"
-//           aria-label="username"
-//           aria-describedby="signIn"
-//         />
-//       </InputGroup>
-
-//       <InputGroup className="mb-3">
-//         <Form.Control
-//           value={user.password}
-//           onChange={(e) => setuser({...user, password: e.target.value})}
-//           onSubmit={login}
-//           placeholder="Password"
-//           aria-label="password"
-//           aria-describedby="signIn"
-//         //   משפט טרנרי בטייפ בשביל לעשות שאפשר לראות
-//           type='password'
-//         />
-//       </InputGroup>
-//          {/* is href ok? */}
-//          </Form.Group>
-//       <Button id='signIn' type='submit' variant="info">Sign In</Button>
-//       </Form>
-// //<Button type='submit' variant="primary" onClick={toggleCard}>Register</Button>
-// return (
-//     <div>
-//       {isCard ? (
-//         //sign in
-//         <div className = 'centerDiv' >
-//           <Card style={{ width: '18rem' }}>
-//             <Card.Body>
-//               <h1 className='bigTitle'>Bomboclat</h1>
-//               <Card.Subtitle className="mb-2 text-muted">Sign In</Card.Subtitle>
-//               <Card.Title>
-//                 Username
-//               </Card.Title>
-            
-//               <InputGroup className="mb-3">
-//                 <Form.Control
-//                 value={user.userName}
-//                 onChange={(e) => setuser({...user, username: e.target.value})}
-//                 placeholder="Username"
-//                 aria-label="Username"
-//                 aria-describedby="signIn"
-//                 />
-//                 <br/>
-//               </InputGroup>
-
-//               <Card.Title>
-//                 Password
-//               </Card.Title>
-//               <Form onSubmit={login}>
-//                 <Form.Group>
-//                   <InputGroup className="mb-3">
-//                     <Form.Control
-//                     value={user.password}
-//                     onChange={(e) => setuser({...user, password: e.target.value})}
-//                     placeholder="Password"
-//                     aria-label="Password"
-//                     aria-describedby="signIn"  
-//                     type={PassowrdVis}
-//                     />
-//                     <Button onMouseDown={()=> setPassVIs('text')} onMouseUp={()=> setPassVIs('password')}>o</Button>
-//                   </InputGroup> 
-              
-//                   <Button id='signIn' type='submit' onClick={login}  variant="info" disabled={isDisabled}>Sign In</Button>
-//                 </Form.Group>
-//               </Form>
-//               <br/><br/>
-//               <Button type='submit' variant="primary" onClick={toggleCard}>Register</Button>
-            
-      
-//             </Card.Body>
-//           </Card>
-//         </div>
-
-//       ):(
-//         //register
-//       <div className='centerDiv'>
-//         <Card style={{ width: '18rem' }}>
-//           <Card.Body>
-//             <h1 className='bigTitle'>Bomboclat</h1>
-//             <Card.Subtitle className="mb-2 text-muted">Register</Card.Subtitle>
-            
-//             <Card.Title>
-//               Please enter a username
-//             </Card.Title>
-//             <Form onSubmit={login}>
-//               <Form.Group>
-//                 <InputGroup className="mb-3">
-//                   <Form.Control
-//                   value={userRegister.userName}
-//                   onChange={(e) => setuserRegister({...user, username: e.target.value})}
-//                   placeholder="Username"
-//                   aria-label="Username"
-//                   aria-describedby="register"
-//                   />
-//                   <br/>
-//                 </InputGroup>
-
-//                 <Card.Title>
-//                   Please enter a password
-//                 </Card.Title>
-
-//                 <InputGroup className="mb-3">
-//                   <Form.Control
-//                   value={user.Password}
-//                   onChange={(e) => setuser({...user, password: e.target.value})}
-//                   placeholder="Password"
-//                   aria-label="Password"
-//                   aria-describedby="register"  
-//                   type={PassowrdVis}
-//                   />
-//                   <Button onMouseDown={()=> setPassVIs('text')} onMouseUp={()=> setPassVIs('password')}>o</Button>
-//                 </InputGroup>
-
-              
-//                 <Button id='register' type='submit' onClick={register} variant="primary">Register</Button>
-//               </Form.Group>
-//             </Form>
-//             <br/><br/>
-//             <Button type='submit' onClick={toggleCard}  variant="info">Sign In</Button>
-          
-        
-//           </Card.Body>
-//         </Card>
-//       </div>
-//       )}
-//     </div>
-//   )
-// }
-
-// export default LoginPage
-
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Listening on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    isOnline: {type: Boolean, default: false},
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  isOnline: { type: Boolean, default: false },
 });
 
-const User = model('User',userSchema);
+const User = model("User", userSchema);
 export default User;
